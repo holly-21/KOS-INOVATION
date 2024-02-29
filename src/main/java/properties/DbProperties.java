169b6a4677250d6ae1435cc0c341f87b45770a3b
holly-21 <holly31521@gmail.com>
2024-02-29 10:38:36 +0900

    settings

--- src/main/java/properties/DbProperties.java
+++
@@ -1,9 +0,0 @@
-package properties;
-
-public interface DbProperties {
-    public static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
-    String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    String USER_ID = "?";
-    String USER_PASS = "?";
-    String test="";
-}

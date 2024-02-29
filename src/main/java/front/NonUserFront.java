7e1b287a2f372f060091074fda807c25fd18017f
jaehyeongP <asso03089@naver.com>
2024-02-29 11:37:31 +0900

    fix NonUserFront.java NonUserFront.java 수정했습니다.

--- src/main/java/front/NonUserFront.java
+++ src/main/java/front/NonUserFront.java
@@ -14,7 +14,8 @@ public class NonUserFront {
             System.out.println("              │              1.로그인 || 2. 회원가입 || 3.충전소검색 || 4.종료                  │ ");
             System.out.println("              └────────────────────────────────────────────────────────────────────────────┘  ");
 
-            System.out.println("  ┌===========================================┐" + "         ┌==      이번주 충전소 사용량 TOP 10       ");
+            System.out.println("  ┌===========================================┐" + "         ┌===========================================┐");
+            System.out.println("           이번주 저렴한 충전소TOP 1       " + "                        이번주 충전소 사용량 TOP 10       ");
             for (int i = 0; i <= 10; i++) {
                 System.out.println("             " + i + "순위" + "~~~ 충전소        " +
                         "  " + "                                   " + i + "순위" + "~~~ 충전소             " + "  ");
@@ -25,9 +26,12 @@ public class NonUserFront {
             int select = sc.nextInt();
             switch (select) {
                 case 1:
+                    System.out.println("로그인 서비스 입니다.");
                     System.out.println("아이디를 입력해주세요:");
-                    //SelectById;
-                    String id = sc.next();
+                    String Checkid = sc.next();
+                    //SelectById(Checkid) 해서 return이 null 이면 회원이 아니므로 로그인 실패.
+
+
 
                     System.out.println("패스워드를 입력해주세요:");
                     //SelectByPwd;
@@ -35,6 +39,27 @@ public class NonUserFront {
 
                     //입력된값을 로그인 함수에 입력해서 로그인 함수 호출.
                     front2.front2();
+
+                case 2:
+                    System.out.println("회원가입 서비스입니다");
+                    System.out.println("아이디를 입력해주세요");
+                    String id= sc.next();
+                    //SelectById(id) 해서 null이면 패스워드 입력받고, null 이 아니면
+                    // 중복된 회원이라고 메세지 출력.
+                    System.out.println("비밀번호를 입력해주세요");
+                    String pw= sc.next();
+                case 3:
+                    System.out.println("              ┌────────────────────────────────────────────────────────────────────────────┐");
+                    System.out.println("              │                               충전소 검색 서비스 입니다                        │ ");
+                    System.out.println("              │                        검색하고자 하는 충전소의 지역명을 적어주세요                │ ");
+                    System.out.println("              │                                ex ) : '강남구 '  ,                          │ ");
+                    System.out.println("              │       '서울'로 입력하면 서울 지역 전체 충전소가 나오기 떄문에 추천드리지 않습니다.      │ ");
+                    System.out.println("              └────────────────────────────────────────────────────────────────────────────┘ ");
+                    System.out.println("검색어를 입력해주세요: ");
+                    String loc = sc.next();
+
+
+
                 case 4:
                     state = false;
                     System.out.println("서비스를 종료합니다.");

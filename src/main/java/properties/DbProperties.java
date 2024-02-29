b6d74110351e9a70209838231c18b2488c17716d
holly-21 <holly31521@gmail.com>
2024-02-29 10:33:38 +0900

    git ignore

--- src/main/java/properties/DbProperties.java
+++ src/main/java/properties/DbProperties.java
@@ -4,6 +4,6 @@ public interface DbProperties {
     public static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
     String URL = "jdbc:oracle:thin:@localhost:1521:xe";
     String USER_ID = " ? ";
-    String USER_PASS = " ? ";
+    String USER_PASS = "?";
 
 }

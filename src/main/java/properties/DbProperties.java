8878de8a1f4549abaea3e9f4681d92b6a624ea6a
holly-21 <holly31521@gmail.com>
2024-02-29 23:24:08 +0900

    update properties

---
+++ src/main/java/properties/DbProperties.java
@@ -0,0 +1,8 @@
+package properties;
+
+public interface DbProperties {
+    public static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
+    String URL = "jdbc:oracle:thin:@kosinovation_high?TNS_ADMIN=/Users/holly/workspace/KOS-INOVATION/Wallet_KOSINOVATION";
+    String USER_ID = "admin";
+    String USER_PASS = "zYMRiP395qFDpJ!";
+}

1c8df1c8633e58c994cb33b25727b55025685f87
jaehyeongP <asso03089@naver.com>
2024-03-05 10:47:15 +0900

    이거저거 많이고침

--- src/main/java/backend/model/session/Session.java
+++ src/main/java/backend/model/session/Session.java
@@ -1,8 +1,41 @@
 package backend.model.session;
 
-/**
- * 회원 사용자
- */
-
 public class Session {
+    private String sessionId;
+
+    public Session() {}
+    public Session(String sessionId) {
+        this.sessionId = sessionId;
+    }
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    //추가
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    @Override
+    public int hashCode() {
+        return sessionId.hashCode();
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        Session other = (Session) obj;
+        if(sessionId.equals(other.sessionId)) {
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+
+
+
+
+
 }
--- src/main/java/backend/service/UsersService.java
+++ src/main/java/backend/service/UsersService.java
@@ -9,6 +9,24 @@ public class UsersService {
 
     UsersDao usersDao =new UsersDaoImpl();
 
+
+    public boolean signUp(String id, String pw, String name) throws  SQLException {
+
+        int signUp = usersDao.signUp(id,pw,name);
+        System.out.println(signUp);
+
+
+
+
+        System.out.println(id+pw);
+
+
+
+
+        return true;
+    }
+
+
     public boolean duplicateCheck(String checkId) throws SQLException {
 
         System.out.println("testing"+checkId);
